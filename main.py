5c8ed3aa58e4416009eaf3bdfa96381d495fca9e
mrhcjones <150591975+mrhcjones@users.noreply.github.com>
2023-12-30 14:06:01 +0000

    Fix Github Secrets related error

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import requests
 mastodon_instance_url = 'https://botsin.space'
 
 # Retrieve secrets from GitHub
-access_token = os.getenv('ACCESS_TOKEN')
-wordnik_api_key = os.getenv('WORDNIK_API_KEY')
+access_token = os.environ('ACCESS_TOKEN')
+wordnik_api_key = os.environ('WORDNIK_API_KEY')
 
 # Wordnik API endpoint for the word of the day
 wordnik_url = f'http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key={wordnik_api_key}'
