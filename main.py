d8851f18665907b00aeb293c53fdf47d4e0c5ed2
Harley Jones <150591975+harleyjones@users.noreply.github.com>
2024-10-14 19:21:40 +0000

    FORMATTING: update post text to include hashtags!

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ try:
     definition = definitions[index]
 
     # Format the toot
-    toot = f"📚 Word of the day: {word}\n\nDefinition(s):\n{definition}"
-    print(toot)
+    toot = f"📚 The word of the day is {word}!\n––––––––––\nDefinition/s:\n{definition}\n#wordoftheday #wotd #english #bot"
     if (debugTesting == 0):
         # Mastodon API endpoint for posting a status
         toot_url = f"{mastodon_instance_url}/api/v1/statuses"
